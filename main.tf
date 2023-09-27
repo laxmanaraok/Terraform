8157929949327c57b4051109097ad4ac11054902
laxmanaraok <116637058+laxmanaraok@users.noreply.github.com>
2023-09-27 15:13:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "test_instance" {
 
-  ami                    = "ami-03a6eaae9938c858c" #linux for testing
+  ami                    = "ami-03a6eaae9938c858c" #Amazon_AMI for testing
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0a48ba209d204976c"]   #default SQ group
   subnet_id              = "subnet-0c78b680152c9090c" #resource-1 subnet
