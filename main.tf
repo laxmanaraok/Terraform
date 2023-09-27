b3723df6ced5f1d45ec5bcefc02f26b9ae0afd89
laxmanaraok <116637058+laxmanaraok@users.noreply.github.com>
2023-09-27 15:09:49 +0530

    updated ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "test_instance" {
 
-  ami                    = "ami-0cdb8266fcd5d3d63" #centos8
+  ami                    = "ami-03a6eaae9938c858c" #linux for testing
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0a48ba209d204976c"]   #default SQ group
   subnet_id              = "subnet-0c78b680152c9090c" #resource-1 subnet
